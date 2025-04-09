53d743210db4f7136fa551d59864ced05836402a
michel-sudo <michelelias288@gmail.com>
2025-04-09 09:06:53 -0300

    finalização

--- src/main/java/BST.java
+++ src/main/java/BST.java
@@ -8,16 +8,15 @@ public class BST {
     private int size;
 
     public boolean isAVL() {
-        //TODO: implementar
-        return false;
+        return Math.abs(balance(root)) <= 1;
     }
 
     /**
      * Retorna a altura da árvore.
      */
     public int height() {
-        //TODO implementar
-        return -1;
+        if (root == null) return -1;
+        return height(this.root);
     }
 
     /**
@@ -25,11 +24,13 @@ public class BST {
      * para recursão e para o balance.
      */
     private int height(Node node) {
-        return -1;
+        if (node == null) return 0;
+        return 1 + Math.max(height(node.left), height(node.right));
     }
 
     private int balance(Node node) {
-        return -1;
+        if (node == null) return 0;
+        return height(node.left) - height(node.right);
     }
 
     /**
